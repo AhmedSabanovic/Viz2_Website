cfddc4af1662e74010902d550eb196a7e74a2a7f
Ahmed Šabanović <92882375+AhmedSabanovic@users.noreply.github.com>
2025-01-07 20:20:41 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,28 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    new Swiper('.swiper-container', {
-      loop: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      },
-      effect: 'coverflow',
-      coverflowEffect: {
-        rotate: 30,
-        slideShadows: false,
-      },
-    });
-  
     document.querySelector('.btn').addEventListener('click', (e) => {
       e.preventDefault();
-      const aboutSection = document.querySelector('#about');
+      const aboutSection = document.querySelector('#Description');
       const headerHeight = document.querySelector('header').offsetHeight;
       const extraOffset = 100; 
       const offsetPosition = aboutSection.offsetTop - headerHeight - extraOffset;
